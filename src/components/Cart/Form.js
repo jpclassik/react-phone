02b2cedce304ae1da455e9bcf0c0072afd571e20
Jpclassik <ordjp54@gmail.com>
2022-04-28 05:53:31 -0700

    committed pages-1

--- src/components/Cart/Form.js
+++ src/components/Cart/Form.js
@@ -4,44 +4,20 @@ import CartTotals from './CartTotals';
 
 export default function Form() {
   return (
-    <div>
-  <form name='contact v1' 
-  method="post" 
-  data-netlify="true" 
-  onSubmit="submit">
-
- <input type="hidden" name="form" value="contact v1" />
-
-     <div>
-       <label>Full Name <br />
-         <input type="text" name="first-name" />
-         </label> 
-
-     </div>
-
-     <div>
-       <label htmlFor='email'>
-         Email 
-       </label> <br/>
-       <input id='email' type="email" name="email"/>
-     </div>
-
-     <div>
-       <label htmlFor='number'>Phone Number</label> <br />
-       <input id='number' type="number" name='number' />
-     </div>
- <div>
-   <label>Current Address</label> <br />
-   <textarea name='address' >
-
-   </textarea>
- </div>
-    <ButtonContainer type='submit'
-    onClick={CartTotals}
-    >submit</ButtonContainer>
-
+    <form name="contact" netlify>
+    <p>
+      <label>Name <br/> <input type="text" name="name" />
+      
+      </label>
+    </p>
+    <p>
+      <label htmlFor='email'>Email <br /> <input type="email" name="email" /></label>
+    </p>
+    <p>
+      <ButtonContainer
+      onClick={CartTotals}
+      type="submit">Submit</ButtonContainer>
+    </p>
   </form>
-
-    </div>
   )
 }
